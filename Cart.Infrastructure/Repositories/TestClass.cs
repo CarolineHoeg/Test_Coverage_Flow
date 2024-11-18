4742a781ab43b789836934935aa9617a17f1d60d
unknown <carolinehoeg123@gmail.com>
2024-11-18 10:04:26 +0100

    Test in other project

---
+++ Cart.Infrastructure/Repositories/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart.Infrastructure.Repositories
+{
+    public class TestClass
+    {
+        public TestClass() { }
+
+        public string Echo(string message)
+        {
+            return message;
+        }
+    }
+}
